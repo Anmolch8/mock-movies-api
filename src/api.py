71b8cdf4d75baae2b3d96040600cb9af629c17e9
Angadjit <Angad38.agcs@gmail.com>
2022-04-30 14:49:25 +0530

    updated file

--- src/api.py
+++ src/api.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 from json import load
+
 
 # movies dict
 movies = None
@@ -7,16 +8,24 @@ movies = None
 def load_json_dict():
     global movies
 
-    with open('json/movies.json','r') as json_file:
+    with open('json/movies.json','r',encoding='utf-8') as json_file:
         movies = load(json_file)
 
 api = Flask(__name__)
 
 @api.route('/get_movies')
 def get_movies():
-    return {
-        'movies' : movies 
-    }
+    return jsonify(movies)
+
+@api.route('/search')
+def search_movie():
+    sr = str(request.args['sr']).replace('+',' ')
+
+
+    filtered_results = list(filter(lambda data:data['movie_name'].startswith(sr) or data['movie_name'].endswith(sr),movies))
+
+    return jsonify(filtered_results)
+
 
 
 if __name__ == '__main__':
