c9ad9fca5f08397be358d016ac2ed43db9729a65
Anmol chawla <chawllaa.anmol@gmail.com>
2022-05-02 20:40:35 +0530

    changes on file location

--- src/api.py
+++ src/api.py
@@ -28,6 +28,3 @@ def search_movie():
 
 
 
-if __name__ == '__main__':
-    load_json_dict()
-    api.run(host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    load_json_dict()
+    api.run(host='0.0.0.0')
